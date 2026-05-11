99a85bde339b5117fc22fb8e6ba64d084cd354ea
Aaron Bataeff <aaron.bataeff.21@gmail.com>
2026-05-10 20:35:54 -0700

    added additional move validation tests

--- CheckersTests/CheckersTest.cs
+++ CheckersTests/CheckersTest.cs
@@ -81,10 +81,81 @@ public class CheckersTests
             for (int c = 0; c < 8; c++)
                 game.board[r, c] = 0;
 
-        game.board[1, 2] = 1; // red piece near top
+        game.board[1, 2] = 1;
 
         game.TryMove(1, 2, 0, 3);
 
-        Assert.That(game.board[0, 3], Is.EqualTo(3)); // king
+        Assert.That(game.board[0, 3], Is.EqualTo(3));
+    }
+
+    [Test]
+    public void RegularPiece_CannotMoveBackwards()
+    {
+        var game = new CheckersGame();
+
+        for (int r = 0; r < 8; r++)
+            for (int c = 0; c < 8; c++)
+                game.board[r, c] = 0;
+
+        game.board[5, 0] = 1;
+
+        game.TryMove(5, 0, 6, 1);
+
+        Assert.That(game.board[5, 0], Is.EqualTo(1));
+        Assert.That(game.board[6, 1], Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Piece_CannotMoveOntoOccupiedSpace()
+    {
+        var game = new CheckersGame();
+
+        for (int r = 0; r < 8; r++)
+            for (int c = 0; c < 8; c++)
+                game.board[r, c] = 0;
+
+        game.board[5, 0] = 1;
+        game.board[4, 1] = 2;
+
+        game.TryMove(5, 0, 4, 1);
+
+        Assert.That(game.board[5, 0], Is.EqualTo(1));
+        Assert.That(game.board[4, 1], Is.EqualTo(2));
+    }
+
+    [Test]
+    public void King_CanMoveBackwards()
+    {
+        var game = new CheckersGame();
+
+        for (int r = 0; r < 8; r++)
+            for (int c = 0; c < 8; c++)
+                game.board[r, c] = 0;
+
+        game.board[3, 2] = 3;
+
+        game.TryMove(3, 2, 4, 3);
+
+        Assert.That(game.board[4, 3], Is.EqualTo(3));
+        Assert.That(game.board[3, 2], Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Piece_CannotCaptureOwnTeam()
+    {
+        var game = new CheckersGame();
+
+        for (int r = 0; r < 8; r++)
+            for (int c = 0; c < 8; c++)
+                game.board[r, c] = 0;
+
+        game.board[5, 0] = 1;
+        game.board[4, 1] = 1;
+
+        game.TryMove(5, 0, 3, 2);
+
+        Assert.That(game.board[5, 0], Is.EqualTo(1));
+        Assert.That(game.board[4, 1], Is.EqualTo(1));
+        Assert.That(game.board[3, 2], Is.EqualTo(0));
     }
 }
